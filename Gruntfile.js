31c9484c0d724b0570df8f322a55c1ee525e823e
Vitalie Maldur <maldur.vitalik@gmail.com>
2014-05-31 00:47:58 +0300

    Clean up front-end

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,6 +57,7 @@ module.exports = function (grunt) {
     'linker/js/app.js',
 
     // *->    put other dependencies here   <-*
+    "/linker/js/jquery-1.11.1.min.js",
 
     // All of the rest of your app scripts imported here
     'linker/**/*.js'
---
+++ assets/linker/js/helpers.js
@@ -0,0 +1,103 @@
+function stack_cards(id_container) {
+  var count  = $(id_container).children().length;
+  var width  = $(id_container).width();
+  var height = $(id_container).height();
+  var card_width = $(id_container).children().first().width();
+  var all_cards_width = card_width + ((count - 1) * card_width / 2);
+  var offset = (width - all_cards_width) / 2;
+  var array_img = $(id_container).children();
+  $(id_container).children().first().css("margin-left", offset);
+  for(var i = 1; i < count; i++) {
+    var img = array_img[i];
+    $(img).css("margin-left", -1 * card_width / 2);
+  }
+}
+
+function updateScoreBoardNames(game_ins) {
+  for(var i = 0; i < game_ins.users_names.length; i++) {
+    $("#player" + (i + 1)).html('<span>' + game_ins.users_names[i] + '</span>');
+  }
+}
+
+function updateActiveUser(game_ins, user_id) {
+  $("#active").remove();
+  var index = game_ins.users_ids.indexOf(user_id);
+  $("#player" + (index + 1)).append('<span id="active" class="glyphicon glyphicon-eye-open"></span>');
+}
+
+function display_buttons(first_char, play, id, user_id) {
+  $('#play-btn').hide();
+
+  $("#pass-btn").click(function() {
+    console.log("da");
+    socket.post("/game/round2", {game: id, user: user_id, action: "pass"}, function(data) {
+      //if is passing
+    });
+    $('#play-btn').show();
+    $("#round2-chooser").empty();
+    $('#first-round-btns').css("visibility", "hidden");
+  });
+
+  if(play) {
+    $("#pass-btn").hide();
+  }
+
+  var div = $("#round2-chooser");
+  if(first_char != 'S')
+    div.append('<button id="spades" type="button"  class="btn btn-default">' +
+               '<span class="card-type" style="color:black">♠</span></button>');
+  if(first_char != 'H')
+    div.append('<button id="hearts" type="button" class="btn btn-default">' +
+          '<span class="card-type" style="color:red">♥</span></button>');
+  if(first_char != 'C')
+    div.append('<button id="clubs" type="button" class="btn btn-default">' +
+          '<span class="card-type" style="color:black">♣</span></button>');
+  if(first_char != 'D')
+    div.append('<button id="diamonds" type="button" class="btn btn-default">' +
+          '<span class="card-type" style="color:red">♦</span></button>');
+
+  div.ready(function() {
+    $("#clubs").click(function() {
+      socket.post("/game/round2", {game: id, user: user_id, action: "play", trump: "C"}, function(data) {
+      //if is playing
+      });
+      $('#play-btn').show();
+      $("#round2-chooser").empty();
+      $('#first-round-btns').css('visibility', 'hidden');
+      if(play) $("#pass-btn").show();
+    });
+
+    $("#spades").click(function() {
+      socket.post("/game/round2", {game: id, user: user_id, action: "play", trump: "S"}, function(data) {
+      //if is playing
+      });
+      $('#play-btn').show();
+      $("#round2-chooser").empty();
+      $('#first-round-btns').css('visibility', 'hidden');
+      if(play) $("#pass-btn").show();
+    });
+
+    $("#hearts").click(function() {
+      socket.post("/game/round2", {game: id, user: user_id, action: "play", trump: "H"}, function(data) {
+      //if is playing
+      });
+      $('#play-btn').show();
+      $("#round2-chooser").empty();
+      $('#first-round-btns').css('visibility', 'hidden');
+      if(play) $("#pass-btn").show();
+    });
+
+    $("#diamods").click(function() {
+      socket.post("/game/round2", {game: id, user: user_id, action: "play", trump: "D"}, function(data) {
+      //if is playing
+      });
+      $('#play-btn').show();
+      $("#round2-chooser").empty();
+      $('#first-round-btns').css('visibility', 'hidden');
+      if(play) $("#pass-btn").show();
+    });
+
+    $('#first-round-btns').css('visibility', 'visible');
+
+  });
+}
